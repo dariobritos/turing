7e215c0c5838af569a1b89fa2e99c0625965f6f8
dario <dario.britos@gmail.com>
2017-07-12 00:23:04 -0300

    Commit inicial

---
+++ rest-api/src/main/java/org/proygrad/turing/restapi/app/App.java
@@ -0,0 +1,21 @@
+package org.proygrad.turing.restapi.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.jetty.JettyEmbeddedServletContainerFactory;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@ComponentScan("org.proygrad.turing.restapi")
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+
+
+}
