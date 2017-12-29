cfa8ba3a84c1efb8fbddbdf1311bc280fb515180
dario.britos <dario.britos@gmail.com>
2017-12-28 22:30:43 -0300

    refactor

--- src/main/java/org/proygrad/turing/restapi/app/App.java
+++ src/main/java/org/proygrad/turing/restapi/app/App.java
@@ -2,9 +2,7 @@ package org.proygrad.turing.restapi.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.embedded.jetty.JettyEmbeddedServletContainerFactory;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
