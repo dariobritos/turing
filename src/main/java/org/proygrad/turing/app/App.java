635f7b35c9218d4c87c9f2763070001223ac5e71
dario.britos <dario.britos@gmail.com>
2018-02-22 23:20:30 -0300

    Docker y amazon

--- src/main/java/org/proygrad/turing/app/App.java
+++ src/main/java/org/proygrad/turing/app/App.java
@@ -5,11 +5,12 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
-// @EnableDiscoveryClient
+@EnableDiscoveryClient
 @ComponentScan("org.proygrad.turing")
 public class App {
 
